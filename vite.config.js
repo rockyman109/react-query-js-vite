42ad722f991a7aa07f8491f32886b7c3ef0de238
rockyman109 <82431601+rockyman109@users.noreply.github.com>
2024-06-27 15:48:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:4000',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   build: {
     outDir: 'dist',
   },
